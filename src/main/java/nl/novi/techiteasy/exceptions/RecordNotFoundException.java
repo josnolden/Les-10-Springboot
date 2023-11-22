ec81d745196694510987931a8ac61276ecb6f6c2
Jos Nolden <jnolden@snelstart.nl>
2023-11-22 14:24:59 +0100

    now with saved changes

--- src/main/java/nl/novi/techiteasy/exceptions/RecordNotFoundException.java
+++ src/main/java/nl/novi/techiteasy/exceptions/RecordNotFoundException.java
@@ -1,2 +1,11 @@
-package nl.novi.techiteasy.exceptions;public class RecordNotFoundException {
+package nl.novi.techiteasy.exceptions;
+
+public class RecordNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+    public RecordNotFoundException() {
+        super();
+    }
+    public RecordNotFoundException(String message) {
+        super(message);
+    }
 }
